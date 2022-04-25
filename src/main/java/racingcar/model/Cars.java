9213b8d87e2667d52bb5e8e50985f8dabbc309b2
JangDongGyu <cdgu1129@knou.ac.kr>
2022-04-25 16:41:49 +0900

    feat: 랜덤으로 차량의 동작여부를 결정하는 기능 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -2,6 +2,8 @@ package racingcar.model;
 
 import java.util.List;
 
+import racingcar.GameUtils;
+
 public class Cars {
 	private List<Car> cars;
 
@@ -11,5 +13,15 @@ public class Cars {
 
 	public List<Car> getCars() {
 		return cars;
+	}
+	
+	public void move() {
+		for(Car car: cars) {
+			car.move(randomCarStatus());
+		}
+	}
+	
+	private CarStatus randomCarStatus() {
+		return GameUtils.getCarStatus(GameUtils.pickNumberInRange());
 	}
 }
