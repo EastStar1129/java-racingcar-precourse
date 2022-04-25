f310c2021600275a025a21a9c653603071fd59b6
JangDongGyu <cdgu1129@knou.ac.kr>
2022-04-25 15:34:14 +0900

    feat: 0~9사이의 랜덤값을 구하고 0~3일경우 멈춤, 4~9일경우 전진하는 기능 구현

---
+++ src/main/java/racingcar/GameUtils.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.exception.CustomIllegalArgumentException;
+import racingcar.model.CarStatus;
+
+public class GameUtils {
+	public static final int MIN_NO = 0;
+	public static final int MID_NO = 4;
+	public static final int MAX_NO = 9;
+	
+	public static CarStatus getCarStatus(int pickNumber) {
+		validatePickNumber(pickNumber);
+		if(pickNumber < MID_NO) {
+			return CarStatus.STOP;
+		}
+		return CarStatus.GO;
+	}
+
+	public static int pickNumberInRange(){
+		return Randoms.pickNumberInRange(MIN_NO, MAX_NO);
+	}
+	
+	public static void validatePickNumber(int pickNumber) {
+		if(pickNumber<MIN_NO || pickNumber>MAX_NO) {
+			throw new CustomIllegalArgumentException(MIN_NO+"부터 "+MAX_NO+"까지 숫자만 입력이 가능합니다.");
+		}
+	}
+	
+}
